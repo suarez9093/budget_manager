1b50632898b481e5e1f743dfbd8d8a8029fd17ac
Alex <suarez9093@hotmail.com>
2019-10-18 19:08:12 -0600

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+// ======================
+// VARIABLES
+// ======================
+
+// 1st: pull initial budgetItems/lastID from localStorage to set initial variables
+
+
+
+// ======================
+// FUNCTIONS
+// ======================
+
+// 4th: function to update localStorage with latest budgetItems and latest lastID
+
+
+// 5th: function to render budgetItems on table; each item should be rendered in this format:
+// <tr data-id="2"><td>Oct 14, 2019 5:08 PM</td><td>November Rent</td><td>Rent/Mortgage</td><td>1300</td><td>Fill out lease renewal form!</td><td class="delete"><span>x</span></td></tr>
+// also, update total amount spent on page (based on selected category):
+
+
+
+// ======================
+// MAIN PROCESS
+// ======================
+
+// 2nd: wire up click event on 'Enter New Budget Item' button to toggle display of form
+
+
+// 3rd: wire up click event on 'Add Budget Item' button, gather user input and add item to budgetItems array
+// (each item's object should include: id / date / name / category / amount / notes)... then clear the form
+// fields and trigger localStorage update/budgetItems rerender functions, once created
+
+
+// 6th: wire up change event on the category select menu, show filtered budgetItems based on selection
+
+
+// 7th: wire up click event on the delete button of a given row; on click delete that budgetItem
+
+
+
+
+
+
